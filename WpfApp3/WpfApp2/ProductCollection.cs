c4347780b1faec12dfc6c86d5e0a3b3b7e0a4660
VYanhy <52915624+VYanhy@users.noreply.github.com>
2020-11-22 10:15:20 +0200

    Update ProductCollection.cs

--- WpfApp3/WpfApp2/ProductCollection.cs
+++ WpfApp3/WpfApp2/ProductCollection.cs
@@ -16,10 +16,11 @@ namespace WpfApp2
             Add(new Product() { ProductId = 3, ProductName = "Adobe XD" });
             Add(new Product() { ProductId = 4, ProductName = "Balsamiq Mockups" });
             Add(new Product() { ProductId = 5, ProductName = "Cleanmock" });
-            /*
+            
             Add(new Product() { ProductId = 6, ProductName = "Figma" });
             Add(new Product() { ProductId = 7, ProductName = "OmniGraffle" });
             Add(new Product() { ProductId = 8, ProductName = "Sketch" });
+            /*
             Add(new Product() { ProductId = 9, ProductName = "Smartmockups" });
             Add(new Product() { ProductId = 10, ProductName = "UXPin" });
             */
